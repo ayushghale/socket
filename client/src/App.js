9e64632929d89f6d32297eb1dacef4cb091108e4
Ayush Gurung <grgakash465671@gmail.com>
2023-10-11 11:43:08 +0545

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -30,8 +30,8 @@ function App() {
   }
 
   return (
-    <div className="text-2xl flex flex-col">
-      <h1 className="text-5xl underline underline-offset-2 mx-auto">
+    <div className="flex flex-col text-2xl">
+      <h1 className="mx-auto text-5xl underline underline-offset-2">
         {" "}
         Chat Room
       </h1>
@@ -43,29 +43,29 @@ function App() {
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className=" border-solid border-4 border-black"
+            className="border-4 border-black border-solid "
           />
         </div>
         <div className="flex flex-col w-[50%] mx-auto border border-solid border-4 border-black p-4 rounded-xl">
           <div className="h-[800px] flex flex-col overflow-y-scroll  gap-y-2">
             {messageDb.map((item, index) => (
-              <div key={index} className=" ">
+              <div key={index} className="">
                 {console.log(item.text)}
                 {name === item.name ? (
-                  <div className="w-fit ml-auto flex">
-                    <div className="flex flex-col bg-blue-500 rounded-2xl rounded-br-none  px-4 py-2">
-                      <h1 className="font-bold text-lg">{`${item.name}`}</h1>
-                      <p className="left-0 text-2xl w-fit ml-auto mt-1">
+                  <div className="flex ml-auto w-fit">
+                    <div className="flex flex-col px-4 py-2 bg-blue-500 rounded-br-none rounded-2xl">
+                      <h1 className="text-lg font-bold">{`${item.name}`}</h1>
+                      <p className="left-0 mt-1 ml-auto text-2xl w-fit">
                         {item.text}
                       </p>
                     </div>
                     icon
                   </div>
                 ) : (
-                  <div className="w-fit mr-auto flex">
-                    <div className="flex flex-col bg-gray-500 rounded-2xl rounded-br-none  px-4 py-2">
-                      <h1 className="font-bold text-lg">{`${item.name}`}</h1>
-                      <p className="left-0 text-2xl w-fit ml-auto mt-1">
+                  <div className="flex mr-auto w-fit">
+                    <div className="flex flex-col px-4 py-2 bg-gray-500 rounded-br-none rounded-2xl">
+                      <h1 className="text-lg font-bold">{`${item.name}`}</h1>
+                      <p className="left-0 mt-1 ml-auto text-2xl w-fit">
                         {item.text}
                       </p>
                     </div>
@@ -76,7 +76,7 @@ function App() {
             ))}
             {/* display message */}
           </div>
-          <div className="border border-solid border-4  border-black  p-2 rounded-xl  w-full  flex">
+          <div className="flex w-full p-2 border border-4 border-black border-solid rounded-xl">
             <input
               type="text"
               value={message}
@@ -89,7 +89,7 @@ function App() {
                 e.preventDefault();
                 sendMessage();
               }}
-              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded shadow right-0 "
+              className="right-0 px-4 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded shadow hover:bg-blue-400 hover:border-blue-500 "
             >
               Send
             </button>
